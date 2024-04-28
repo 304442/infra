78aa28e96e3f19de9482ed36275a2eb255cd1df4
root <root@vmi1686791.contaboserver.net>
2024-04-28 11:08:37 -0500

    updated scripts

--- destroy_rke2.sh
+++ destroy_rke2.sh
@@ -5,6 +5,7 @@ sed -i '/export PATH=\$PATH:\/var\/lib\/rancher\/rke2\/bin/d' "$HOME/.bashrc"
 
 # Remove export KUBECONFIG="/etc/rancher/k3s/k3s.yaml" if present
 sed -i '/export KUBECONFIG="\/etc\/rancher\/rke2\/rke2.yaml"/d' "$HOME/.bashrc"
+unset KUBECONFIG
 
 source ~/.bashrc
 
