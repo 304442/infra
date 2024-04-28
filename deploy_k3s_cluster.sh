473ce00bd2317c98d583b203666e21ce163eae94
abdurrehman11 <abdurrehman245@gmail.com>
2024-04-28 19:59:08 +0500

    destroy k3s script

--- deploy_k3s_cluster.sh
+++ deploy_k3s_cluster.sh
@@ -3,25 +3,18 @@
 # Install k3s and configure kubeconfig
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644
 
-# Check k3s service status
-# systemctl status k3s
-
-# Check kubectl version
-/usr/local/bin/kubectl version
-
-# Check current PATH
 echo $PATH
 
-# Add /usr/local/bin to PATH if it's not already in it
 if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
-    export PATH=$PATH:/usr/local/bin
+    echo "export PATH=$PATH:/usr/local/bin" >> "$HOME/.bashrc"
 fi
 
-# Check kubectl version again after updating PATH
+if [ -z "$KUBECONFIG" ] || [ "$KUBECONFIG" != "/etc/rancher/k3s/k3s.yaml" ]; then
+    export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+    echo 'export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"' >> "$HOME/.bashrc"
+fi
+
+source ~/.bashrc
+
 kubectl version
-
-# Get nodes in the cluster
 kubectl get nodes
-
-# Get namespaces in the cluster
-kubectl get ns
